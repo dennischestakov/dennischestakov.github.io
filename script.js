1de5750067a650d857d2f91bdbf2943c8a18785d
Dennis Chestakov <30273683+ScoutyCodes@users.noreply.github.com>
2018-02-25 12:12:31 -0800

    Added Experience Section

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop(); // how many pixels you've scrolled
+  var os = $('#introSection').offset().top; // pixels to the top of introSection
+  var ht = $('#introSection').height(); // height of introSection in pixels
+
+  if(scroll > os + ht + 100){
+    $('#navBar').addClass('solid');
+  }
+
+  if(scroll < os + ht + 100){
+    $('#navBar').removeClass('solid');
+  }
+});
